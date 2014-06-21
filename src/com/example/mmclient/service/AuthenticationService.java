9754bfadcfbbf5f39ae543b1a61ea489fca0e289
swapnil <swapnil.raipur@gmail.com>
2014-06-21 15:29:59 +0530

    Added login screen and submit amount screen

---
+++ src/com/example/mmclient/service/AuthenticationService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.example.mmclient.service;
+
+/**
+ * Common Interface for all Authentication services.
+ * 
+ * @author SWAPNIL
+ * 
+ */
+public interface AuthenticationService {
+	boolean authenticate(String username, String password);
+}
---
+++ src/com/example/mmclient/service/GoogleAuthenticationService.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.example.mmclient.service;
+
+/**
+ * Service doing Authentication using Google APIS.
+ * 
+ * @author SWAPNIL
+ * 
+ */
+public class GoogleAuthenticationService implements AuthenticationService {
+
+	@Override
+	public boolean authenticate(String username, String password) {
+		// TODO authenticate using Google APIS
+		return true;
+	}
+
+}
